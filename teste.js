680ef4bcda1a9ec638db6f120b4eebcdbe5ab745
Bernardo <sardinhaber@gmail.com>
2024-03-19 10:50:08 -0300

    Entrega final da atividade

---
+++ teste.js
@@ -0,0 +1,13 @@
+let frase = 'O rato roeu a roupa do rei de roma!';
+let minuscula =  frase.toLowerCase();
+let separaçao = minuscula.split("");
+let vogais = ['a', 'e', 'i', 'o', 'u']
+let qualVogal = [];
+
+
+
+for (let i = 0; i < separaçao.length; i++) 
+
+   
+
+console.log(qualVogal)
