bd7ab425f6c73da491ed974f06814fab53f2a95c
Bernardo <sardinhaber@gmail.com>
2024-03-19 11:20:15 -0300

    Finalização do teste

--- teste.js
+++ teste.js
@@ -5,9 +5,14 @@ let vogais = ['a', 'e', 'i', 'o', 'u']
 let qualVogal = [];
 
 
+for (let i = 0; i < separaçao.length; i++){
+    let letra = separaçao[i];
+    if(vogais.includes(letra)) {
+        qualVogal.push(letra)
+      
+    }
+}  
 
-for (let i = 0; i < separaçao.length; i++) 
-
-   
-
-console.log(qualVogal)
+console.log("O texto é : " + frase)
+console.log("As vogais são : " + qualVogal.toString())
+console.log("O número de vogal é : " + qualVogal.length )
