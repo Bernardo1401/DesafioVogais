ebdcb821cd4a764bdb33f5578a711344139032fc
Bernardo <sardinhaber@gmail.com>
2024-03-19 10:32:22 -0300

    Teste

--- exercicio01.js
+++ exercicio01.js
@@ -1,5 +1,13 @@
-let nome = ["hello"]
-let resultado = nome.map(nome => {
-    nome == e
-})
-console.log(resultado)
+let frase = 'O rato roeu a roupa do rei de roma!';
+let minuscula = frase.toLowerCase();
+let separaçao = minuscula.split("");
+let vogais = ['a', 'e', 'i', 'o', 'u'];
+
+let vogaisEncontradas = separaçao.filter(letra => vogais.includes(letra));
+let numero = vogaisEncontradas.length;
+
+console.log("A frase é : " + frase);
+console.log("As vogais encontradas são : " + vogaisEncontradas);
+console.log("O número total de vogais é : " + numero);
+    
+
